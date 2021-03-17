8685a02672747633a95cee6f4ac657579be7ef7b
Yemi <y.kehinde@hotmail.com>
2021-03-17 20:09:54 +0000

    started conditional render

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [submit, setSubmit] = useState(false);
+
+  function handleSubmit() {
+    setSubmit(true)
+  }
+
+  function backToForm() {
+    setSubmit(false)
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Bookmarker</h1>
       </header>
+      <main>
+        {!submit? 
+        <form>
+          <input type="text" placeholder="Enter a URL" />
+          <button onClick={handleSubmit}>Submit</button>
+        </form>
+        :
+        <div>
+          Thank you for your submission
+          <button onClick={backToForm}>Back to form</button>
+        </div>
+        }
+      </main>
     </div>
   );
 }
